4eb6f8980ec44c6d068850e7c23a046d0ee47543
Jadis <IceColdRhino@users.noreply.github.com>
2025-08-22 09:55:26 -0500

    De-neuter Crafting Tabs
    All tabs besides "Compare Jobs" were disabled, along with several corresponding queries, due to lag related to claim size. There are some changes upstream that should help with this. Undid the disabling that I did, in preparation for a merge from upstream.

--- app/client/query_service.py
+++ app/client/query_service.py
@@ -255,28 +255,28 @@ class QueryService:
                 "WHERE building_state.claim_entity_id = '{claim_id}';".format(claim_id=claim_id)
             ),
             # Get active crafting state for users in the claim
-            # (
-            #     "SELECT progressive_action_state.* "
-            #     "FROM progressive_action_state "
-            #     "JOIN building_state "
-            #     "ON progressive_action_state.building_entity_id = building_state.entity_id "
-            #     "WHERE building_state.claim_entity_id = '{claim_id}';".format(claim_id=claim_id)
-            # ),
+            (
+                "SELECT progressive_action_state.* "
+                "FROM progressive_action_state "
+                "JOIN building_state "
+                "ON progressive_action_state.building_entity_id = building_state.entity_id "
+                "WHERE building_state.claim_entity_id = '{claim_id}';".format(claim_id=claim_id)
+            ),
             # Get active crafting buildings in claim if accept help is turned on
-            # (
-            #     "SELECT public_progressive_action_state.* "
-            #     "FROM public_progressive_action_state "
-            #     "JOIN building_state "
-            #     "ON public_progressive_action_state.building_entity_id = building_state.entity_id "
-            #     "WHERE building_state.claim_entity_id = '{claim_id}';".format(claim_id=claim_id)
-            # ),
+            (
+                "SELECT public_progressive_action_state.* "
+                "FROM public_progressive_action_state "
+                "JOIN building_state "
+                "ON public_progressive_action_state.building_entity_id = building_state.entity_id "
+                "WHERE building_state.claim_entity_id = '{claim_id}';".format(claim_id=claim_id)
+            ),
             # Get passive crafting state for buildings in the claim
-            # (
-            #     "SELECT passive_craft_state.* "
-            #     "FROM passive_craft_state "
-            #     "JOIN building_state ON passive_craft_state.building_entity_id = building_state.entity_id "
-            #     "WHERE building_state.claim_entity_id = '{claim_id}';".format(claim_id=claim_id)
-            # ),
+            (
+                "SELECT passive_craft_state.* "
+                "FROM passive_craft_state "
+                "JOIN building_state ON passive_craft_state.building_entity_id = building_state.entity_id "
+                "WHERE building_state.claim_entity_id = '{claim_id}';".format(claim_id=claim_id)
+            ),
             # Get buy order state for orders placed within the claim
             (
                 "SELECT buy_order_state.* "
